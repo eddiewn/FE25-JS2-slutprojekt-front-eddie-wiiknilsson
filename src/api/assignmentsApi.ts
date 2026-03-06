fc943ea356875df2565d7ef5abc7137ebc24a1df
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-06 11:19:23 +0100

    a

--- src/api/assignmentsApi.ts
+++ src/api/assignmentsApi.ts
@@ -1,58 +1,60 @@
 import type { Assignments } from "../Types";
 
-export const getAssignments = async() => {
-    try {
-        const URL = "http://localhost:3000/getAssignments"
-        const response = await fetch(URL);
+export const getAssignments = async () => {
+  try {
+    const URL = "http://localhost:3000/getAssignments";
+    const response = await fetch(URL);
 
-        if(!response.ok) throw new Error(`Fel någonstans att skicka assignment, ${response.status}`)
+    if (!response.ok)
+      throw new Error(
+        `Fel någonstans att skicka assignment, ${response.status}`,
+      );
 
-        const result = await response.json()
-        return result;
-    } catch (error) {
-        console.log(error)
-    }
-}
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const removeAssignment = async(id: string) => {
-    try {
-        const URL = "http://localhost:3000/deleteAssignment"
+export const removeAssignment = async (id: string) => {
+  try {
+    const URL = "http://localhost:3000/deleteAssignment";
 
-        const response = await fetch(URL, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                id
-        })
-        })
+    const response = await fetch(URL, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    });
 
-        const result = await response.json();
-        console.log(result)
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const patchAssignment = async(assignment: Assignments) => {
-    try {
-        const URL = "http://localhost:3000/patchAssignment"
-        const response = await fetch(URL, {
-            method: "PATCH",
-            headers:{
-                "Content-Type": "application/json",         
-            },
-            body: JSON.stringify(assignment)
-        })
+export const patchAssignment = async (assignment: Assignments) => {
+  try {
+    const URL = "http://localhost:3000/patchAssignment";
+    const response = await fetch(URL, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(assignment),
+    });
 
-        const result = await response.json();
+    const result = await response.json();
 
-        if(!result.ok) throw new Error ("fel");
+    if (!result.ok) throw new Error("fel");
 
-        console.log(result)
-    } catch (error) {
-        console.log(error)
-    }
-}
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
